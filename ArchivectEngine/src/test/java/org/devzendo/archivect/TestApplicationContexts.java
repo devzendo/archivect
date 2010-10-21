8dc9692137cf386fb71746af4efa874b095d741e
Matt Gumbley <matt.gumbley@devzendo.org>
2010-10-21 07:43:40 +0100

    The engine now has application contexts, and code to load them.

---
+++ ArchivectEngine/src/test/java/org/devzendo/archivect/TestApplicationContexts.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (C) 2008-2010 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.devzendo.archivect;
+
+import org.apache.log4j.BasicConfigurator;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+
+/**
+ * Can the Archivect Application Contexts be loaded successfully?
+ * @author matt
+ *
+ */
+public final class TestApplicationContexts {
+    /**
+     * 
+     */
+    @BeforeClass
+    public static void logSetup() {
+        BasicConfigurator.configure();
+    }
+    
+    /**
+     * 
+     */
+    @Test
+    public void loadSpringLoaderOK() {
+        new ArchivectSpringLoaderInitialiser().getSpringLoader();
+        // shouldn't throw!
+    }
+}
