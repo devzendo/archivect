6b41ce926baf4c1732e5665275cb342b0dcfbc61
Matt Gumbley <matt.gumbley@devzendo.org>
2010-10-22 18:36:25 +0100

    Ensure that XPFSA is available to the engine code.

---
+++ ArchivectEngine/src/test/java/org/devzendo/xpfsa/SpaceInvader.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright (C) 2008-2010 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.devzendo.xpfsa;
+
+/**
+ * Logs natively via the package-private method of XPFSA, hence why it's in
+ * this package, named thusly.
+ * 
+ * @author matt
+ *
+ */
+public class SpaceInvader {
+    /**
+     * Log a message natively
+     * @param message the message
+     * @param fsa the library
+     */
+    public static void logMessage(final String message, final FileSystemAccess fsa) {
+        fsa.logDebugNative(message);
+    }
+}
