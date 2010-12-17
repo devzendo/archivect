1a55ed402e3f1eb2c73583f0f7dc4927dd9cb48c
Matt Gumbley <matt.gumbley@devzendo.org>
2010-12-17 07:52:38 +0000

    Added a scala version of the ArchivectMainFrame

---
+++ ArchivectUI/src/main/scala/org/devzendo/archivect/gui/SArchivectMainFrame.scala
@@ -0,0 +1,74 @@
+/**
+ * Copyright (C) 2008-2010 Matt Gumbley, DevZendo.org <http://devzendo.org>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+ 
+package org.devzendo.archivect.gui
+
+import java.awt.BorderLayout
+import java.awt.Dimension
+import java.awt.event.WindowAdapter
+import java.awt.event.WindowEvent
+
+import javax.swing.JFrame
+import javax.swing.JPanel
+import javax.swing.WindowConstants
+
+import org.apache.log4j.Logger
+import org.devzendo.commonapp.gui.WindowGeometryStore
+import org.devzendo.commoncode.resource.ResourceLoader
+
+object SArchivectMainFrame {
+    private val LOGGER = Logger.getLogger(classOf[SArchivectMainFrame])
+    private val MAIN_FRAME_NAME = "main"
+}
+
+/**
+ * The Archivect UI Main Frame, saves its geometry, and handles closing.
+ * Construct the main application frame, given the geometry store in which
+ * its initial size and location will be loaded, and persisted on close.
+ * 
+ * @author matt
+ * @param windowGeometryStore the geometry store
+ */
+class SArchivectMainFrame(windowGeometryStore: WindowGeometryStore) extends JFrame {
+    setIconImage(ResourceLoader.createResourceImageIcon("org/devzendo/archivect/icons/application16x16.gif").getImage())
+    setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE)
+
+    setName(SArchivectMainFrame.MAIN_FRAME_NAME)
+    setLayout(new BorderLayout())
+
+    // TODO inject me....
+    val mainPanel = new JPanel() // TODO to be replaced by the main app panel
+    mainPanel.setPreferredSize(new Dimension(640, 480))
+    add(mainPanel, BorderLayout.CENTER)
+        
+    loadInitialGeometry
+    setupGeometrySaveOnMoveOnClose
+
+    def loadInitialGeometry = {
+        if (!windowGeometryStore.hasStoredGeometry(this)) {
+            pack
+        }
+        windowGeometryStore.loadGeometry(this)
+    }
+    
+    def setupGeometrySaveOnMoveOnClose = {
+        addWindowListener(new WindowAdapter() {
+            override def windowClosed(e: WindowEvent ) {
+                SArchivectMainFrame.LOGGER.info("Saving geometry")
+                windowGeometryStore.saveGeometry(SArchivectMainFrame.this)
+        }});
+    }
+}
