f2187c9963ddf09857fbdbb2448a99007375cfc4
Matt Gumbley <matt.gumbley@devzendo.org>
2011-03-25 22:10:43 +0000

    The destinations editor is now functional, but very ugly.

--- ArchivectUI/src/main/scala/org/devzendo/archivect/gui/editor/DestinationEditorDialog.scala
+++ ArchivectUI/src/main/scala/org/devzendo/archivect/gui/editor/DestinationEditorDialog.scala
@@ -29,79 +29,97 @@ import org.devzendo.archivect.gui.SwingImplicits._
 import org.devzendo.commonapp.gui.GUIUtils
 
 object DestinationEditorDialog {
-    val LocalPanelName = "Local"
-    val SmbPanelName = "Smb"
+    val LocalPanelName = "Local Disk"
+    val SmbPanelName = "Windows Share"
 }
 class DestinationEditorDialog(val parentFrame: Frame) extends JDialog(parentFrame, true) {
     setTitle("Add destination") // TODO: change to edit if editing
     // Handle window closing correctly.
     //setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
 
-    val formLayout = new FormLayout("pref, 4dlu, 50dlu",    // columns
-                                    "pref, 2dlu, pref, 2dlu, pref")    // rows
+    val cc = new CellConstraints()
+    val topFormLayout = new FormLayout("40dlu, 4dlu, 80dlu",    // columns
+                                       "pref, 2dlu, pref, 2dlu, pref")    // rows
     val rowGroups: Array[Array[Int]] = Array[Array[Int]](Array(1, 3, 5))
-    formLayout.setRowGroups(rowGroups)
+    topFormLayout.setRowGroups(rowGroups)
     
+    getContentPane().setLayout(new BorderLayout(20, 20))
     val enclosingPanel = new JPanel()
-    setLayout(new BoxLayout(enclosingPanel, BoxLayout.Y_AXIS))
+    enclosingPanel.setLayout(new BoxLayout(enclosingPanel, BoxLayout.Y_AXIS))
     add(enclosingPanel, BorderLayout.CENTER)
 
-    val cc = new CellConstraints()
-    val topPanel = new JPanel(formLayout)
+    // Top panel
+    val topPanel = new JPanel(topFormLayout)
 
     topPanel.add(new JLabel("Name:"), cc.xy(1, 1))
     val nameLabel = new JTextArea()
-    topPanel.add(nameLabel, cc.xyw(3, 1, 3))
+    topPanel.add(nameLabel, cc.xy(3, 1))
     
     topPanel.add(new JLabel("Type:"), cc.xy(1, 3))
-    private val types = Array[Object]("Local Disk", "Windows Share")
-    private val type2Panel = Map.empty[Object, JPanel]
+    private val types = Array[Object](DestinationEditorDialog.LocalPanelName, DestinationEditorDialog.SmbPanelName)
     private val typeCombo = new JComboBox(types)
-    topPanel.add(typeCombo, cc.xyw(3, 3, 3))
+    topPanel.add(typeCombo, cc.xy(3, 3))
     
     enclosingPanel.add(topPanel)
 
+    // Card layout
     private val cardLayout = new CardLayout()
     private val cardPanel = new JPanel(cardLayout)
-    enclosingPanel.add(cardPanel)
-    
-    private val localPanel = new JPanel(gridLayout())
-    localPanel.add(new JLabel("Path:"))
+
+    // Local card
+    val localFormLayout = new FormLayout("40dlu, 4dlu, 80dlu",    // columns
+                                         "pref")    // rows
+    val localRowGroups: Array[Array[Int]] = Array[Array[Int]](Array(1))
+    localFormLayout.setRowGroups(localRowGroups)
+    val lcc = new CellConstraints()
+
+    private val localPanel = new JPanel(localFormLayout)
+    localPanel.add(new JLabel("Path:"), lcc.xy(1, 1))
     private val localPath = new JTextArea()
-    localPanel.add(localPath)
+    localPanel.add(localPath, lcc.xy(3, 1))
     
     cardPanel.add(localPanel, DestinationEditorDialog.LocalPanelName)
     
-    private val smbPanel = new JPanel(gridLayout())
-    smbPanel.add(new JLabel("Path:"))
+    // SMB card
+    val smbFormLayout = new FormLayout("40dlu, 4dlu, 80dlu",    // columns
+                                       "pref, 2dlu, pref, 2dlu, pref, 2dlu, pref, 2dlu, pref")    // rows
+    val smbRowGroups: Array[Array[Int]] = Array[Array[Int]](Array(1, 3, 5, 7, 9))
+    smbFormLayout.setRowGroups(smbRowGroups)
+    val scc = new CellConstraints()
+
+    private val smbPanel = new JPanel(smbFormLayout)
+    smbPanel.add(new JLabel("Path:"), scc.xy(1, 1))
     private val smbPath = new JTextArea()
-    smbPanel.add(smbPath)
-    smbPanel.add(new JLabel("User name:"))
+    smbPanel.add(smbPath, scc.xy(3, 1))
+    smbPanel.add(new JLabel("User name:"), scc.xy(1, 3))
     private val smbUser = new JTextArea()
-    smbPanel.add(smbUser)
-    smbPanel.add(new JLabel("Password:"))
+    smbPanel.add(smbUser, scc.xy(3, 3))
+    smbPanel.add(new JLabel("Password:"), scc.xy(1, 5))
     private val smbPassword = new JTextArea()
-    smbPanel.add(smbPassword)
-    smbPanel.add(new JLabel("Server:"))
+    smbPanel.add(smbPassword, scc.xy(3, 5))
+    smbPanel.add(new JLabel("Server:"), scc.xy(1, 7))
     private val smbServer = new JTextArea()
-    smbPanel.add(smbServer)
-    smbPanel.add(new JLabel("Share:"))
+    smbPanel.add(smbServer, scc.xy(3, 7))
+    smbPanel.add(new JLabel("Share:"), scc.xy(1, 9))
     private val smbShare = new JTextArea()
-    smbPanel.add(smbShare)
+    smbPanel.add(smbShare, scc.xy(3, 9))
     
     cardPanel.add(smbPanel, DestinationEditorDialog.SmbPanelName)
+
+    enclosingPanel.add(cardPanel)
     
     typeCombo.addItemListener((_ : ItemEvent) => {
         val selected = typeCombo.getSelectedItem()
-        cardLayout.show(type2Panel(selected), selected.toString())
+        println(selected)
+        cardLayout.show(cardPanel, selected.toString())
     })
     
     private val validationPanel = new JPanel(new FlowLayout())
     private val validationProblems = GUIUtils.createNonEditableJTextAreaWithParentBackground(validationPanel)
-    validationProblems.setText("  ");
+    validationProblems.setText("Status:  ");
     enclosingPanel.add(validationPanel)
     
-    enclosingPanel.add(new JSeparator(SwingConstants.HORIZONTAL))
+    enclosingPanel.add(new JSeparator(SwingConstants.HORIZONTAL)) // ?
     
     private val buttonBox = new Box(BoxLayout.X_AXIS)
     private val testButton = new JButton("Test access")
@@ -113,6 +131,4 @@ class DestinationEditorDialog(val parentFrame: Frame) extends JDialog(parentFram
     buttonBox.add(cancelButton)
     
     enclosingPanel.add(buttonBox)
-    
-    private def gridLayout(): GridLayout = new GridLayout(4, 2, 16, 16)
 }
