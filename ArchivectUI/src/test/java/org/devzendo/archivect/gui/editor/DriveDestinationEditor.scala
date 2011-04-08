0408d121fe217578c97e0bb816086914d874c621
Matt Gumbley <matt.gumbley@devzendo.org>
2011-04-08 08:35:04 +0100

    Now closes properly and removes listener from destinations

--- ArchivectUI/src/test/java/org/devzendo/archivect/gui/editor/DriveDestinationEditor.scala
+++ ArchivectUI/src/test/java/org/devzendo/archivect/gui/editor/DriveDestinationEditor.scala
@@ -2,9 +2,11 @@ package org.devzendo.archivect.gui.editor
 
 import collection.JavaConversions._
 
-import java.awt.{ BorderLayout, FlowLayout }
-import java.awt.event.{ ActionListener, ActionEvent }
-import javax.swing.{ JLabel, JFrame, JButton, JPanel, WindowConstants }
+import java.awt.{BorderLayout, FlowLayout}
+import java.awt.event.{ActionListener, ActionEvent, WindowAdapter, WindowEvent}
+import javax.swing.{JLabel, JFrame, JButton, JPanel, WindowConstants}
+
+import org.apache.log4j.Logger
 
 import org.devzendo.commonapp.prefs._
 
@@ -18,6 +20,9 @@ import org.devzendo.archivect.destinations._
 import org.devzendo.archivect.gui.SwingImplicits._
 
 object DriveDestinationEditor {
+    private val LOGGER = Logger.getLogger(classOf[DestinationEditor])
+    // not sure why this can't be this class?!
+    
     def main(args: Array[String]): Unit = {
         val logging = Logging.getInstance()
         logging.setupLoggingFromArgs(args.toList)
@@ -26,6 +31,7 @@ object DriveDestinationEditor {
             def run = {
                 ThreadCheckingRepaintManager.initialise();
                 Beautifier.makeBeautiful();
+                DriveDestinationEditor.LOGGER.info("Starting driver")
 
                 val destinations = loadDestinations()
                 
@@ -49,10 +55,19 @@ object DriveDestinationEditor {
                 })
                 destEditor.startEditing() // trigger empty message
     
-                // TODO: call destEditor.stopEditing() when wizard closes.
+    
+                frame.addWindowListener(new WindowAdapter() {
+                    override def windowClosing(e: WindowEvent) {
+                        DriveDestinationEditor.LOGGER.info("Detected window closing; removing listeners")
+                        destEditor.stopEditing()
+                        frame.dispose()
+                    }
+                    override def windowClosed(e: WindowEvent ) {
+                        DriveDestinationEditor.LOGGER.info("Detected window closed")
+                }})
     
                 frame.pack()
-                frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE)
+                frame.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE)
                 frame.setVisible(true)
             }
         })
