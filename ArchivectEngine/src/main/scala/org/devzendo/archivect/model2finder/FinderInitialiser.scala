c173325ff20430a454141ae860eafa9ae24377b0
Matt Gumbley <matt.gumbley@devzendo.org>
2011-09-16 08:25:22 +0100

    Add arguments to the FinderInitialiser

--- ArchivectEngine/src/main/scala/org/devzendo/archivect/model2finder/FinderInitialiser.scala
+++ ArchivectEngine/src/main/scala/org/devzendo/archivect/model2finder/FinderInitialiser.scala
@@ -1,5 +1,12 @@
 package org.devzendo.archivect.model2finder
 
-class FinderInitialiser {
+import org.devzendo.archivect.model.CommandModel
+import org.devzendo.archivect.finder.Finder
+import org.devzendo.archivect.rule.RuleCompiler
 
+class FinderInitialiser(ruleCompiler: RuleCompiler, finder: Finder) {
+    def populateFromModel(model: CommandModel) {
+        // TODO Auto-generated method stub
+        
+    }
 }
